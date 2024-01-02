44e1a7af463be23fc61f98d8bc7909b1c7b5707b
Denis Stebunov <denis.stebunov@ivelum.com>
2024-01-02 21:44:10 +0100

    fix look for the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,37 +1,22 @@
 import { Link } from 'gatsby';
 import React from 'react';
 
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
+import GenericLayout from '../layout/GenericLayout';
 
-const paragraphStyles = {
-  marginBottom: 48,
-};
-
-function NotFoundPage() {
+export default function NotFoundPage() {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
+    <GenericLayout>
+      <h1>Page not found</h1>
+      <p>
         Sorry 😔, we couldn’t find what you were looking for.
         <br />
         <br />
         <Link to="/">Go home</Link>
         .
       </p>
-    </main>
+    </GenericLayout>
   );
 }
-
-export default NotFoundPage;
 
 export function Head() {
   return <title>Not found</title>;
