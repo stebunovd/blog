8400a5922356ea9e3061a359a6ddfc761fbe5df0
Denis Stebunov <denis.stebunov@ivelum.com>
2024-01-14 22:09:57 +0200

    trying to fix headings for Instapaper

--- static/prettify.js
+++ static/prettify.js
@@ -8,7 +8,7 @@ function activateAnchors() {
   document.querySelectorAll('h2').forEach(function (h) {
     var id = h.id;
     if (id) {
-      h.innerHTML = '<a href="#' + id + '" class="anchor"><span>' + h.textContent + '</span></a>';
+      h.innerHTML = '<a href="#' + id + '" class="anchor">' + h.innerHTML + '</a>';
     }
   });
 }
