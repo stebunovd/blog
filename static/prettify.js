2f4d842d06ecd089c00c6e8559a0f63ac8901b54
Denis Stebunov <denis.stebunov@ivelum.com>
2024-01-14 22:05:53 +0200

    fix headings in Safari Reader

--- static/prettify.js
+++ static/prettify.js
@@ -5,14 +5,10 @@ function activateAnchors() {
   if (!article) {
     return;
   }
-  var headings = document.querySelectorAll('h2').forEach(function (h) {
+  document.querySelectorAll('h2').forEach(function (h) {
     var id = h.id;
     if (id) {
-      var a = document.createElement('a');
-      a.href = '#' + id;
-      a.className = 'anchor';
-      a.text = h.innerHTML;
-      h.innerHTML = a.outerHTML;
+      h.innerHTML = '<a href="#' + id + '" class="anchor"><span>' + h.textContent + '</span></a>';
     }
   });
 }
